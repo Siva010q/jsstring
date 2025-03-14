e57b2048970c7e21581d19dbe4348deb95b00046
siva010q <suryaarts110@gmail.com>
2025-03-14 16:35:37 +0530

    javascript strings

---
+++ string.js
@@ -0,0 +1,21 @@
+let abcd = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("sting").innerHTML = abcd.length;
+
+let word = "HELLO WORLD";
+document.getElementById("demo").innerHTML = word.charCodeAt(0);
+
+
+let fruit = "Apple, Banana, Kiwi";
+let part = fruit.slice(7,13);
+document.getElementById("demo").innerHTML = part;
+
+
+let text = "Hello world, welcome to the universe.";
+document.getElementById("tf").innerHTML = text.startsWith("Hello");
+
+
+
+
+
+
+
